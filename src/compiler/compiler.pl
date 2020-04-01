a1bf86c29b99ddf5e1a4d85765595a51efdb491f
Sukhpreet Singh Anand <sukhpreet.singhanand@gmail.com>
2020-04-01 01:55:58 -0700

    Create required files in each directory

---
+++ src/compiler/compiler.pl
@@ -0,0 +1,7 @@
+% @author Sukhpreet Singh Anand
+% @author Sakshi Jain
+% @author Aditya Bajaj
+% @author Aihaab Shaikh
+% @version 1.0
+% @purpose A lexer to parse the source file and generate tokens and parser to consume the tokens to generate the parse tree
+% @date 04/01/2020
---
+++ src/runtime/runtime.pl
@@ -0,0 +1,7 @@
+% @author Sukhpreet Singh Anand
+% @author Sakshi Jain
+% @author Aditya Bajaj
+% @author Aihaab Shaikh
+% @version 1.0
+% @purpose A runtime environment which parses the intermediate abstract syntax tree code and executes the program
+% @date 04/01/2020
